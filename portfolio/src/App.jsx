2f0fb76df0025e04de407b98e9774492a31abb6a
Mohit <mohitt.tech@gmail.com>
2025-11-04 21:26:22 +0530

    Update App.jsx

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import About from './components/About'
+import Skills from './components/Skills'
+import Projects from './components/Projects'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
 
-const App = () => {
-  return (
-    <div>Hello World this is Mohit's portfolio</div>
-  )
+
+export default function App() {
+return (
+<div className="min-h-screen flex flex-col">
+<Navbar />
+<main className="flex-1">
+<Hero />
+<About />
+<Skills />
+<Projects />
+<Contact />
+</main>
+<Footer />
+</div>
+)
 }
-
-export default App
