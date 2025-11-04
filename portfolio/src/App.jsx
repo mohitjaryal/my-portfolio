e08f77ff690d9a5d717b83f50b0c3bf63b14ef06
Mohit <mohitt.tech@gmail.com>
2025-11-04 22:25:20 +0530

    Update App.jsx

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,25 +1,24 @@
-import React from 'react'
-import Navbar from './components/Navbar'
-import Hero from './components/Hero'
-import About from './components/About'
-import Skills from './components/Skills'
-import Projects from './components/Projects'
-import Contact from './components/Contact'
-import Footer from './components/Footer'
-
+import React from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Skills from "./components/Skills";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 export default function App() {
-return (
-<div className="min-h-screen flex flex-col">
-<Navbar />
-<main className="flex-1">
-<Hero />
-<About />
-<Skills />
-<Projects />
-<Contact />
-</main>
-<Footer />
-</div>
-)
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Navbar />
+      <main className="flex-1">
+        <Hero />
+        <About />
+        <Skills />
+        <Projects />
+        <Contact />
+      </main>
+      <Footer />
+    </div>
+  );
 }
